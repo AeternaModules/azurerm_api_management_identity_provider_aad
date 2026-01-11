993fe5b9135836e5c619e2f3bf0840bbeb031b9c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:47:03 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_identity_provider_aads" {
+  description = "All api_management_identity_provider_aad resources"
+  value       = azurerm_api_management_identity_provider_aad.api_management_identity_provider_aads
+}
+
